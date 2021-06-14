c90a5503005d0270467206221c72e20d3541cd6a
Leonardo Bloise <leonardo.bloise@outlook.com>
2021-06-14 12:00:11 -0300

    Inicio do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <form>
+            <input type="text" placeholder="Titulo"></input>
+            <textarea placeholder="Escreva sua nota..."></textarea>
+            <button> Criar Nota </button>
+      </form>
+      <ul>
+        <li>
+            <section> 
+              <header>
+                <h3>
+                  Título
+                </h3>
+                <p>
+                  Escreva sua nota
+                </p>
+              </header>
+            </section>
+        </li>
+      </ul>
+    </section>
+   
   );
 }
 
