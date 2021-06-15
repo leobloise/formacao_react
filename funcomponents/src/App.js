f97b177d98d2453981321f6412e39b59f769fe44
Leonardo Bloise <leonardo.bloise@outlook.com>
2021-06-15 19:53:17 -0300

    Finalizando validações com Material UI

--- funcomponents/src/App.js
+++ funcomponents/src/App.js
@@ -6,9 +6,19 @@ function App() {
   return (
     <Container component="article" maxWidth="sm">
       <Typography variant="h3" align="center" component="h1"> Formulário de cadastro </Typography>
-      <FormularioCadastro></FormularioCadastro>
+      <FormularioCadastro validarCpf={validarCpf} aoEnviar={aoEnviarForm}></FormularioCadastro>
     </Container>
   );
+}
+
+function aoEnviarForm(dados) {
+  console.log(dados)
+}
+
+function validarCpf(cpf) {
+  if(cpf.length < 11 || cpf.length > 11)
+    return({valido: false, texto: "O CPF deve ter 11 digitos"})
+  return({valido: true, texto: ""})
 }
 
 export default App;
--- funcomponents/src/Components/FormularioCadastro/FormularioCadastro.jsx
+++ funcomponents/src/Components/FormularioCadastro/FormularioCadastro.jsx
@@ -3,30 +3,73 @@ import { useState } from 'react';
 
 
 
-export function FormularioCadastro(props) {
+export function FormularioCadastro({aoEnviar, validarCpf}) {
 
     const [nome, setNome] = useState("");
+    const [sobrenome, setSobrenome] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [promocao, setPromocao] = useState(true)
+    const [novidades, setNovidades] = useState(true)
+    const [erros, setErros] = useState({cpf: {
+        valido: true,
+        texto: ""
+    }})
+
 
     return (
-        <form>
+        <form onSubmit={e => {
+            e.preventDefault()
+            aoEnviar({nome, sobrenome, cpf, promocao, novidades})
+        }}>
             <TextField  value ={nome} onChange={(e) => {
                 e.preventDefault();
-
-                setNome(e.target.value)
                 
-                if(e.target.value.length >= 3 ) {
-                    setNome(e.target.value.substr(0, 3))
-                }
+                let tempNome = e.target.value
+
+                setNome(tempNome);
 
             } }margin="normal" id="nome" label="Nome" variant="outlined" fullWidth></TextField>
-            <TextField margin="normal"id="surname" label="Sobrenome" variant="outlined"fullWidth></TextField>
-            <TextField margin="normal" id="cpf" label="CPF" variant="outlined"fullWidth></TextField>
+            <TextField value={sobrenome} onChange={
+                (e => {
+                    e.preventDefault();
+                    setSobrenome(e.target.value)
+                })
+            } margin="normal"id="surname" label="Sobrenome" variant="outlined"fullWidth></TextField>
+            <TextField 
+            error={!erros.cpf.valido}
+            helperText={erros.cpf.texto}
+            onBlur={
+                (e) => {
+                    const ehValido = validarCpf(e.target.value)
+                    setErros({
+                        cpf: ehValido
+                    })
+                }
+            }
+            margin="normal" onChange={
+                (e => {
+                    e.preventDefault();
+                    setCpf(e.target.value)
+                })
+            }id="cpf" label="CPF" variant="outlined"fullWidth></TextField>
             <FormControlLabel
-                control={<Switch name="promocoes"  color="primary" defaultChecked={true}></Switch>}
+                control={<Switch 
+                    onChange={
+                        e => {
+                            setPromocao(e.target.checked)
+                        }
+                    }
+                    name="promocoes"  color="primary" checked={promocao}></Switch>}
                 label="Promoções"
             />
             <FormControlLabel
-                control={<Switch name="novidades"  color="primary" defaultChecked={true}></Switch>}
+                control={<Switch name="novidades"  
+                onChange={
+                    e => {
+                        setNovidades(e.target.checked)
+                    }
+                }
+                color="primary" checked={novidades}></Switch>}
                 label="Novidades"
             />
             <Button type="submit" variant="contained" color="primary"> Cadastrar </Button>
