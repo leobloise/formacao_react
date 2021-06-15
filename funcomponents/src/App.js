7c0abeb153d3e02d1af143eeee5c770b4384b464
Leonardo Bloise <leonardo.bloise@outlook.com>
2021-06-15 19:23:51 -0300

    Usando estado em componentes

--- funcomponents/src/App.js
+++ funcomponents/src/App.js
@@ -1,11 +1,13 @@
 import FormularioCadastro from "./Components/FormularioCadastro"
+import {Container, Typography} from '@material-ui/core';
+import 'fontsource-roboto'
 
 function App() {
   return (
-    <>
-      <h1> Formulário de cadastro </h1>
+    <Container component="article" maxWidth="sm">
+      <Typography variant="h3" align="center" component="h1"> Formulário de cadastro </Typography>
       <FormularioCadastro></FormularioCadastro>
-    </>
+    </Container>
   );
 }
 
--- funcomponents/src/Components/FormularioCadastro/FormularioCadastro.jsx
+++ funcomponents/src/Components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,8 +1,35 @@
+import { TextField, Switch, Button, FormControlLabel } from '@material-ui/core';
+import { useState } from 'react';
+
+
+
 export function FormularioCadastro(props) {
+
+    const [nome, setNome] = useState("");
 
     return (
         <form>
-            <input type="text"/>
+            <TextField  value ={nome} onChange={(e) => {
+                e.preventDefault();
+
+                setNome(e.target.value)
+                
+                if(e.target.value.length >= 3 ) {
+                    setNome(e.target.value.substr(0, 3))
+                }
+
+            } }margin="normal" id="nome" label="Nome" variant="outlined" fullWidth></TextField>
+            <TextField margin="normal"id="surname" label="Sobrenome" variant="outlined"fullWidth></TextField>
+            <TextField margin="normal" id="cpf" label="CPF" variant="outlined"fullWidth></TextField>
+            <FormControlLabel
+                control={<Switch name="promocoes"  color="primary" defaultChecked={true}></Switch>}
+                label="Promoções"
+            />
+            <FormControlLabel
+                control={<Switch name="novidades"  color="primary" defaultChecked={true}></Switch>}
+                label="Novidades"
+            />
+            <Button type="submit" variant="contained" color="primary"> Cadastrar </Button>
         </form>
     )
 
